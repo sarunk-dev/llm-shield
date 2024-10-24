edd4080e436c7b1680b77c198790a88453b736b9
Oliver Luo <oliverzxluo@gmail.com>
2024-10-23 23:15:55 -0700

    fix: add vulnerabilities

--- scraper-gui-app/public/js/summaryPage.js
+++ scraper-gui-app/public/js/summaryPage.js
@@ -1,30 +1,29 @@
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
   try {
     console.log("Fetching scraped data from backend");
-    const response = await fetch('/scrape/get-summary'); 
+    const response = await fetch("/scrape/get-summary");
     const data = await response.json();
-    console.log('Received data:', data);
+    console.log("Received data:", data);
 
     if (data.summaryText) {
-      document.getElementById('text-summary').textContent = data.summaryText;
+      document.getElementById("text-summary").innerHTML = data.summaryText;
     }
 
     if (data.images && data.images.length > 0) {
-      const imageSummary = document.getElementById('image-summary');
-      data.images.forEach(image => {
-        const imgElement = document.createElement('img');
+      const imageSummary = document.getElementById("image-summary");
+      data.images.forEach((image) => {
+        const imgElement = document.createElement("img");
         imgElement.src = image.url;
         imgElement.alt = image.caption;
 
-        const captionElement = document.createElement('p');
-        captionElement.textContent = image.caption;
+        const captionElement = document.createElement("p");
+        captionElement.innerHTML = image.caption;
 
         imageSummary.appendChild(imgElement);
         imageSummary.appendChild(captionElement);
       });
     }
-
   } catch (error) {
-    console.error('Error fetching the summary:', error);
+    console.error("Error fetching the summary:", error);
   }
 });
